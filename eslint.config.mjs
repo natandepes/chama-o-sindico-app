0138d7c9d6fab65c4e3d5286c591472368f7abd1
Natan Depes <natand@4xdigital.ai>
2025-03-18 20:12:40 -0300

    Adds ESLint pre-commit hook for unused vars and explict any declarations

---
+++ eslint.config.mjs
@@ -0,0 +1,43 @@
+import { defineConfig } from 'eslint/config';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import angularEslint from '@angular-eslint/eslint-plugin';
+
+export default defineConfig([
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules', 'dist', 'coverage'],
+
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+
+    plugins: {
+      '@angular-eslint': angularEslint,
+    },
+
+    rules: {
+      'no-unused-vars': 'error',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    ignores: ['node_modules', 'dist', 'coverage'],
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      '@angular-eslint': angularEslint,
+    },
+
+    rules: {
+      'no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+]);
