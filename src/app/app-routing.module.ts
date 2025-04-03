70e0e91a4c414f0ac53d5e4bb2f124b527ba6fd4
lubragon <lubragon@gmail.com>
2025-04-03 20:34:21 -0300

    [FEAT] Complaint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarDenunciaComponent } from './features/Denuncia/criar-denuncia/criar-denuncia.component';
+import { ViewComplaintComponent } from './features/Denuncia/view-complaint/view-complaint.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'criar-denuncia', component: CriarDenunciaComponent }, 
+  { path: 'create-complaint', component: CriarDenunciaComponent }, 
+  { path: 'view-complaint', component: ViewComplaintComponent }, 
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CriarDenunciaComponent } from './features/Denuncia/criar-denuncia/criar-denuncia.component';
+import { ViewComplaintComponent } from './features/Denuncia/view-complaint/view-complaint.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CriarDenunciaComponent
+    CriarDenunciaComponent,
+    ViewComplaintComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
