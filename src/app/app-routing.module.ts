e7e1582af7a90fb988609c9cb5811adfa11524a8
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-04-11 12:07:11 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ViewCondominalManagerContactInfoComponent } from './features/condominal-manager-contact-info/view-condominal-manager-contact-info/view-condominal-manager-contact-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'view-condominal-manager-contact-info', component: ViewCondominalManagerContactInfoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/features/condominal-manager-contact-info/condominal-manager-contact-info.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { ViewCondominalManagerContactInfoComponent } from "./view-condominal-manager-contact-info/view-condominal-manager-contact-info.component";
+
+@NgModule({
+  declarations: [
+    ViewCondominalManagerContactInfoComponent
+  ],
+  imports: [],
+  exports: [
+    ViewCondominalManagerContactInfoComponent
+  ]
+})
+
+export class CondominalManagerContactInfoModule { }
