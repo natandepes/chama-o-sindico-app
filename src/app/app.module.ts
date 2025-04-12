adb2d7b5be06a2f123e9ddabcf8cd428c92eaf03
Natan Depes <natand@4xdigital.ai>
2025-04-11 22:17:21 -0300

    Conserta erros de merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,22 @@ import { AppComponent } from './app.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { CondominialServicesModule } from './features/condominal-services/condominal-services.module';
-import { LoginModule } from './features/authentication/login.module';
+import { AuthenticationModule } from './features/authentication/authentication.module';
 import { ComplaintModule } from './features/complaint/complaint.module';
 import { SharedModule } from './features/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
-    CommonModule, 
-    BrowserModule, 
-    ComplaintModule, 
+    CommonModule,
+    BrowserModule,
+    ComplaintModule,
     CondominialServicesModule,
-    LoginModule, 
-    SharedModule, 
-    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
+    AuthenticationModule,
+    SharedModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [provideHttpClient(withInterceptorsFromDi())],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
+++ src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
@@ -1,36 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CondominalServicesService } from '../../services/condominal-services.service';
-// import { ServicoService } from '../../services/servico.service'; // ajuste o caminho conforme sua estrutura
-
 
 @Component({
   selector: 'app-view-condominal-services',
   templateUrl: './view-condominal-services.component.html',
-  styleUrls: ['./view-condominal-services.component.css'],
-  standalone: false
+  styleUrl: './view-condominal-services.component.scss',
+  standalone: false,
 })
-
 export class ViewCondominalServiceComponent implements OnInit {
-  service: any;
+  service: Record<string, unknown> | null = null;
   isEditing = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    public condominalServicesService: CondominalServicesService
+    public condominalServicesService: CondominalServicesService,
   ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.service = this.condominalServicesService.getService(id ?? '');
-      // console.log('Serviço carregado:', this.service);
     }
   }
-  
+
   toEdit() {
-    this.router.navigate(['/servico', this.service.id, 'editar']);
+    this.router.navigate(['/servico', this.service!['id'], 'editar']);
   }
 
   toggleEditing() {
