9e0c9ea12ada5c99f4900f549ba650f0fa0a2b65
Natan Depes <natandepes@hotmail.com>
2025-04-25 20:13:35 -0300

    Adds contact info on routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ViewCondominalServiceComponent } from './features/condominal-services/c
 import { CreateComplaintComponent } from './features/complaint/components/create-complaint/create-complaint.component';
 import { ViewComplaintComponent } from './features/complaint/components/view-complaint/view-complaint.component';
 import { LoginComponent } from './features/authentication/components/login/login.component';
+import { ContactInfoComponent } from './features/contact-info/components/contact-info/contact-info.component';
 
 export const appRoutes: Route[] = [
   { path: ROUTE_PATHS.login, component: LoginComponent },
@@ -13,5 +14,6 @@ export const appRoutes: Route[] = [
   { path: ROUTE_PATHS.viewCondominalService, component: ViewCondominalServiceComponent },
   { path: ROUTE_PATHS.createComplaint, component: CreateComplaintComponent },
   { path: ROUTE_PATHS.viewComplaint, component: ViewComplaintComponent },
+  { path: ROUTE_PATHS.contactInfo, component: ContactInfoComponent },
   { path: ROUTE_PATHS.wildcard, redirectTo: ROUTE_PATHS.home, pathMatch: 'full' },
 ];
