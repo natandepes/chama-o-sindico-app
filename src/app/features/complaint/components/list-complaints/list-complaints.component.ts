a705e377ef178ba4de8b79b861c3361222a51ff3
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2025-05-02 15:05:27 -0300

    [FIX] Tornando List Complaint Component Funcional

--- src/app/features/complaint/components/list-complaints/list-complaints.component.ts
+++ src/app/features/complaint/components/list-complaints/list-complaints.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ComplaintService } from '../../services/complaint.service';
 import { ComplaintMock, ComplaintStatus } from '../../models/complaint.models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-complaints',
@@ -11,34 +12,80 @@ import { ComplaintMock, ComplaintStatus } from '../../models/complaint.models';
 export class ListComplaintsComponent implements OnInit {
   complaints: ComplaintMock[] = [];
   rawComplaints: ComplaintMock[] = [];
-  constructor(private complaintService: ComplaintService) {}
+  userId: string | null = "";
+  constructor(
+    private complaintService: ComplaintService,
+    private router: Router) 
+  {
+    
+  }
 
   ngOnInit() {
+    this.userId = localStorage.getItem('userId')
+
     this.loadComplaints();
   }
 
   loadComplaints() {
-    this.complaintService.getAllComplaints().subscribe({
-      next: (response: any) => {
-        this.rawComplaints = response?.data ?? [];
 
-        this.complaints = this.rawComplaints.map((c: any) => ({
-          id: c.id,
-          title: c.title,
-          description: c.description,
-          imageUrl: c.imageUrl,
-          status: this.mapStatus(c.status), 
-          createdAt: new Date(c.createdAt),
-          closedAt: c.closedAt ? new Date(c.closedAt) : null,
-          closedByUserId: this.closedByUserId ? null: ''
-        }))
+    if(this.userId == '680c0a04085c7259a0221e5aZ') // Trocar para futura validação de síndico
+    {
 
-      },
-      error: err => {
-        console.error('Erro loading complaints:', err);
-        this.complaints = []; 
-      },
-    });
+      this.complaintService.getAllComplaints().subscribe({
+        next: (response: any) => 
+        {
+          this.rawComplaints = response?.data ?? [];
+  
+          this.complaints = this.rawComplaints.map((c: any) => 
+          ({
+            id: c.id,
+            title: c.title,
+            description: c.description,
+            imageUrl: c.imageUrl,
+            status: this.mapStatus(c.status), 
+            createdAt: new Date(c.createdAt),
+            closedAt: c.closedAt ? new Date(c.closedAt) : null,
+            createdByUserId: c.createdByUserId,
+            closedByUserId: c.closedByUserId ? null: ''
+          }))
+        },
+        error: err => 
+        {
+          console.error('Erro loading complaints:', err);
+          this.complaints = []; 
+        },
+      });
+    }
+    else
+    {
+      this.complaintService.getAllComplaintsByUserId().subscribe({
+        next: (response: any) => 
+        {
+          this.rawComplaints = response?.data ?? [];
+  
+          this.complaints = this.rawComplaints.map((c: any) => 
+          ({
+            id: c.id,
+            title: c.title,
+            description: c.description,
+            imageUrl: c.imageUrl,
+            status: this.mapStatus(c.status), 
+            createdAt: new Date(c.createdAt),
+            closedAt: c.closedAt ? new Date(c.closedAt) : null,
+            createdByUserId: c.createdByUserId,
+            closedByUserId: c.closedByUserId ? null: ''
+          }))
+        },
+        error: err => 
+        {
+          console.error('Erro loading complaints:', err);
+          this.complaints = []; 
+        },
+      });
+    }
+    
+
+
   }
 
   mapStatus(statusNumber: number): ComplaintStatus {
@@ -48,6 +95,11 @@ export class ListComplaintsComponent implements OnInit {
       case 2: return ComplaintStatus.Resolved;
       default: return ComplaintStatus.Pending;
     }
+  }
+  
+  protected goToCreateComplaint()
+  {
+    this.router.navigate(['/complaints/create/'])
   }
   
   
