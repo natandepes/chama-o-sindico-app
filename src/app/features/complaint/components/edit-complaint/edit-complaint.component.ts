833be1ed4577c431d4773a1f409b634595fd33b4
Lucas Braganca Goncalves (Holding DTI) <lucasbg@aguiabranca.com.br>
2025-05-05 14:15:49 -0300

    [FEAT] Criado componente 'Edit Complaint'

---
+++ src/app/features/complaint/components/edit-complaint/edit-complaint.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-complaint',
+  standalone: false,
+  templateUrl: './edit-complaint.component.html',
+  styleUrl: './edit-complaint.component.css'
+})
+export class EditComplaintComponent {
+
+}
