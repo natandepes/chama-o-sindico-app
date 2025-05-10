a0df3593de2dbbf61bf1b16a79531533d5ab5f2a
Natan Depes <natandepes@hotmail.com>
2025-05-09 21:33:06 -0300

    Improves home page

--- src/app/features/home/components/home.component.ts
+++ src/app/features/home/components/home.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { ROUTE_PATHS } from '../../../app.paths';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: false,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  cards = [
+    { icon: '/icons/DocumentIcon.svg', label: 'Documentos', route: ROUTE_PATHS.home },
+    { icon: '/icons/MegaphoneIcon.svg', label: 'Denúncias', route: ROUTE_PATHS.listComplaints },
+    { icon: '/icons/WorkIcon.svg', label: 'Serviços', route: ROUTE_PATHS.createCondominalService },
+    { icon: '/icons/ChatIcon.svg', label: 'Fale com o Síndico', route: ROUTE_PATHS.contactInfo },
+    { icon: '/icons/PoolIcon.svg', label: 'Reservas', route: ROUTE_PATHS.viewReservation },
+    { icon: '/icons/BuildingIcon.svg', label: 'Edifício', route: ROUTE_PATHS.viewArea },
+  ];
 
+  constructor(private router: Router) {}
+
+  routeTo(route: string): void {
+    this.router.navigate([route]);
+  }
 }
