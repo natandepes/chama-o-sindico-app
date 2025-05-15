e4d8f704a9df0ce51031ef5a832e7700ee9ef4d3
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-05-14 21:46:59 -0300

    adding the createdAt on the warnings list

--- src/app/features/warnings/models/warning.model.ts
+++ src/app/features/warnings/models/warning.model.ts
@@ -1,15 +1,9 @@
 export interface WarningModel {
-<<<<<<< HEAD
   id?: string;
-=======
->>>>>>> f1ebdbf (creating view warnings and create warning component)
   title: string;
   description: string;
   targetType: string;
   residentId: string;
   residentUserId: string;
-<<<<<<< HEAD
   createdAt?: Date;
-=======
->>>>>>> f1ebdbf (creating view warnings and create warning component)
 }
