1dd5ae6484c485232aa20f40d8b8d6ee5cdd1dea
Natan Depes <natandepes@hotmail.com>
2025-05-15 21:27:28 -0300

    Improves a lot condominal services screen

--- src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
+++ src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CondominalServicesService } from '../../services/condominal-services.service';
 import { CondominalService } from '../../models/condominal-service.model';
 import { ServiceComment } from '../../models/service-comment.model';
+import { ROUTE_PATHS } from '../../../../app.paths';
 
 @Component({
   selector: 'app-view-condominal-services',
   templateUrl: './view-condominal-services.component.html',
   styleUrl: './view-condominal-services.component.scss',
   standalone: false,
+  encapsulation: ViewEncapsulation.None,
 })
 export class ViewCondominalServiceComponent implements OnInit {
-  service!: CondominalService;
-  comments: Array<ServiceComment> = [];
+  service: CondominalService | null = null;
+  comments: ServiceComment[] = [];
   isCommenting = false;
   isEditing = false;
   newComment = '';
@@ -27,7 +29,7 @@ export class ViewCondominalServiceComponent implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.condominalServicesService.getService(id).subscribe({
-        next: (response) => {
+        next: response => {
           if (response.success) {
             this.service = response.data!;
             this.getComments(this.service.id!);
@@ -35,7 +37,7 @@ export class ViewCondominalServiceComponent implements OnInit {
             console.error('Error fetching service:', response.message);
           }
         },
-        error: (error) => {
+        error: error => {
           console.error('Error fetching service:', error);
         },
       });
@@ -44,43 +46,43 @@ export class ViewCondominalServiceComponent implements OnInit {
 
   getComments(serviceId: string) {
     this.condominalServicesService.getServiceComments(serviceId).subscribe({
-      next: (response) => {
+      next: response => {
         if (response.success) {
           this.comments = response.data!;
         } else {
           console.error('Error fetching comments:', response.message);
         }
       },
-      error: (error) => {
+      error: error => {
         console.error('Error fetching comments:', error);
       },
     });
   }
 
-  addComment(){
+  addComment() {
     this.isCommenting = true;
   }
 
   submitComment() {
     if (this.newComment.trim()) {
       const comment: ServiceComment = {
-        condominalServiceId: this.service.id!,
+        condominalServiceId: this.service!.id!,
         comment: this.newComment,
         createdAt: new Date(),
       };
 
       this.condominalServicesService.createComment(comment).subscribe({
-        next: (response) => {
+        next: response => {
           if (response.success) {
             alert('Comentário adicionado com sucesso!');
-            this.getComments(this.service.id!);
+            this.getComments(this.service!.id!);
             this.newComment = '';
             this.isCommenting = false;
           } else {
             console.error('Error creating comment:', response.message);
           }
         },
-        error: (error) => {
+        error: error => {
           console.error('Error creating comment:', error);
         },
       });
@@ -103,5 +105,9 @@ export class ViewCondominalServiceComponent implements OnInit {
   saveChanges() {
     // Aqui você poderia salvar no serviço, se quiser persistir
     this.isEditing = false;
+  }
+
+  goBack() {
+    this.router.navigate([ROUTE_PATHS.listCondominalService]);
   }
 }
