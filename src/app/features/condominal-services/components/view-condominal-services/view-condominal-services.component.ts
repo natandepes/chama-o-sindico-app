5ef584e7667353c8679358a1ef57fa99f54c3616
TheAbraaonix <carlosabraao@hotmail.com.br>
2025-05-16 18:08:53 -0300

    only a resident can add a comment on the condominal service

--- src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
+++ src/app/features/condominal-services/components/view-condominal-services/view-condominal-services.component.ts
@@ -5,6 +5,8 @@ import { CondominalService } from '../../models/condominal-service.model';
 import { ServiceComment } from '../../models/service-comment.model';
 import { ROUTE_PATHS } from '../../../../app.paths';
 import { LoaderService } from '../../../shared/services/loader.service';
+import { AuthService } from '../../../authentication/services/auth.service';
+import { UserRole } from '../../../authentication/models/user-roles.model';
 
 @Component({
   selector: 'app-view-condominal-services',
@@ -19,16 +21,21 @@ export class ViewCondominalServiceComponent implements OnInit {
   isCommenting = false;
   isEditing = false;
   newComment = '';
+  protected userRole: UserRole | null = null;
+  protected readonly UserRoleEnum = UserRole;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     public condominalServicesService: CondominalServicesService,
     private loader: LoaderService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
+    this.userRole = this.authService.getUserRole();
+    
     if (id) {
       this.loader.show();
       this.condominalServicesService.getService(id).subscribe({
